dabcb84587e708b3f4375ad67d5e797f0ab5acb5
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-14 20:51:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,9 +21,11 @@ class Student extends Person {
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.age} is studying`);
+		console.log(`${this.name} is studying`);
 	}
 }
+const person=new Person("john",30);
+
 
 // Do not change the code below this line
 window.Person = Person;
